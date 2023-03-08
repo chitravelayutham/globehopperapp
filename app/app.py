b5f8fc6131a4468a5e89f2c88aaaecdcde9cddc0
chitravelayutham <chitravelayutham@yahoo.ca>
2023-03-08 11:03:38 -0500

    Added CSRF

--- app/app.py
+++ app/app.py
@@ -3,9 +3,12 @@
 
 from flask import Flask, request, jsonify
 import country
+from flask_wtf.csrf import CSRFProtect
 
 #Using Flask framework
 app = Flask(__name__)
+csrf = CSRFProtect()
+csrf.init_app(app)
 
 #Create - POST API
 @app.post('/countries')
